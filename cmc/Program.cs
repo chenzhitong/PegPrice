62311210213d933e7077b2c992254207b568c13a
陈志同 <chenzhitong12@hotmail.com>
2020-08-11 11:37:03 +0800

    update

--- cmc/Program.cs
+++ cmc/Program.cs
@@ -2,52 +2,61 @@
 using System;
 using System.IO;
 using System.Net;
-using System.Threading;
+using System.Text;
+using System.Timers;
 using System.Web;
 
 namespace cmc
 {
     class Program
     {
-        static string API_KEY;
-        static string NeoURL;
-        static string EthURL;
         static void Main(string[] args)
         {
-            var config = JObject.Parse(File.ReadAllText("config.json"));
-            API_KEY = config["key"].ToString();
-            NeoURL = config["NeoURL"].ToString();
-            EthURL = config["EthURL"].ToString();
+            var t = new Timer(new TimeSpan(0, 10, 0).TotalMilliseconds);
+            t.Elapsed += GetPrice;
+            t.Start();
+            GetPrice(null, null);
 
-            while (true)
+            var t2 = new Timer(new TimeSpan(0, 1, 0).TotalMilliseconds);
+            t2.Elapsed += GetBalance;
+            t2.Start();
+            GetBalance(null, null);
+
+            Console.ReadLine();
+        }
+
+        private static void GetPrice(object sender, ElapsedEventArgs e)
+        {
+            try
             {
-                try
-                {
-                    var usdt_btc = CoinMarketCapAPICall("USDT", "BTC");
-                    var usdt_usd = CoinMarketCapAPICall("USDT", "USD");
-                    var json = new JObject { { "USDT-BTC", usdt_btc }, { "USDT-USD", usdt_usd }, { "DateTime", DateTime.Now.ToString() } };
-                    Console.WriteLine(json);
-                    File.WriteAllText("price.txt", json.ToString());
-                }
-                catch (WebException e)
-                {
-                    Console.WriteLine(e.Message);
-                }
-                try
-                {
-                    var json = new JObject { { "USDT", GetUsdtBalance() }, { "PEG", GetPegBalance() }, { "DateTime", DateTime.Now.ToString() } };
-                    Console.WriteLine(json);
-                    File.WriteAllText("wallet.txt", json.ToString());
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e.Message);
-                }
-                Thread.Sleep(60000); //CoinMarketCap cache / update frequency: Every 60 seconds.
+                var key = JObject.Parse(File.ReadAllText("config.json"))["key"].ToString();
+                var usdt_btc = CoinMarketCapAPICall("USDT", "BTC", key);
+                var usdt_usd = CoinMarketCapAPICall("USDT", "USD", key);
+                var json = new JObject { { "USDT-BTC", usdt_btc }, { "USDT-USD", usdt_usd }, { "DateTime", DateTime.Now.ToString() } };
+                Console.WriteLine(json);
+                File.WriteAllText("price.txt", json.ToString());
+            }
+            catch (WebException ex)
+            {
+                Console.WriteLine($"CoinMarketCapAPICall\t{ex.Message}");
             }
         }
 
-        static string CoinMarketCapAPICall(string token, string convert)
+        private static void GetBalance(object sender, ElapsedEventArgs e)
+        {
+            try
+            {
+                var json = new JObject { { "USDT", GetUsdtBalance() }, { "PEG", GetPegBalance() }, { "DateTime", DateTime.Now.ToString() } };
+                Console.WriteLine(json);
+                File.WriteAllText("wallet.txt", json.ToString());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static string CoinMarketCapAPICall(string token, string convert, string apiKey)
         {
             var URL = new UriBuilder("https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest");
 
@@ -58,7 +67,7 @@ namespace cmc
             URL.Query = queryString.ToString();
 
             var client = new WebClient();
-            client.Headers.Add("X-CMC_PRO_API_KEY", API_KEY);
+            client.Headers.Add("X-CMC_PRO_API_KEY", apiKey);
             client.Headers.Add("Accepts", "application/json");
 
             var result = client.DownloadString(URL.ToString());
@@ -71,14 +80,51 @@ namespace cmc
 
         static string GetPegBalance()
         {
-            var response = new WebClient().DownloadString($"{NeoURL}?jsonrpc=2.0&method=getbalance&params=['075383de6638e042efc4bd3daca4ceb516ec1c6b']&id=1");
-            return JObject.Parse(response)["result"]["balance"].ToString();
+            try
+            {
+                var url = JObject.Parse(File.ReadAllText("config.json"))["NeoURL"].ToString();
+                var response = PostWebRequest($"{url}", "{\"jsonrpc\": \"2.0\",\"method\": \"getPegBalance\",\"params\": [],\"id\": 1}");
+                return JObject.Parse(response)["result"]["balance"].ToString();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"GetPegBalance\t{e.Message}");
+                return "0";
+            }
         }
 
         static string GetUsdtBalance()
         {
-            var response = new WebClient().DownloadString($"{EthURL}/Eth/BalanceOfWallet");
-            return JObject.Parse(response)["data"].ToString();
+            try
+            {
+                var url = JObject.Parse(File.ReadAllText("config.json"))["EthURL"].ToString();
+                var response = new WebClient().DownloadString($"{url}/Eth/BalanceOfWallet");
+                return JObject.Parse(response)["data"].ToString();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"GetUsdtBalance\t{e.Message}");
+                return "0";
+            }
+        }
+
+        public static string PostWebRequest(string postUrl, string paramData)
+        {
+            try
+            {
+                var byteArray = Encoding.UTF8.GetBytes(paramData);
+                var request = WebRequest.Create(postUrl);
+                request.Method = "POST";
+                request.ContentType = "application/json";
+                request.GetRequestStream().Write(byteArray, 0, byteArray.Length);
+                using var response = request.GetResponse();
+                using var sr = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
+                return sr.ReadToEnd();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
     }
 }
